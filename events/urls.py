c1864b72c17bd09fb983f8c1e08601bb8e86fa9e
Hamed Hasan <hamedhasan422421@gmail.com>
2024-02-25 11:47:38 +0300

    🚀 Added routes for creating, accepting, and detailing events in events/urls.py. 📊 Defined paths for creating events, accepting events, and getting event details. 🌟 Implemented URL patterns for EventCreateAPIView, EventAcceptAPIView, and EventDetailAPIView. 🏆 Established endpoints for creating, accepting, and viewing event details. 🔄 Organized URL routing for event-related functionality in events/urls.py.

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,10 @@
+
+
 from django.urls import path
-from .views import EventCreateAPIView, EventAcceptAPIView
+from .views import EventCreateAPIView, EventAcceptAPIView, EventDetailAPIView
 
 urlpatterns = [
     path('create/', EventCreateAPIView.as_view(), name='event_create'),
     path('accept/<int:pk>/', EventAcceptAPIView.as_view(), name='event_accept'),
+    path('detail/<int:pk>/', EventDetailAPIView.as_view(), name='event_detail'),
 ]
