4f9d03f8d76f09b12fe53fd22b33a1bb45ef23af
Hamed Hasan <hamedhasan422421@gmail.com>
2024-02-29 19:16:46 +0300

    🚀 Added pagination controls for better user navigation. 📊 Implemented pagination logic to handle large datasets efficiently. 🌟 Updated pagination links for clearer user experience. 🏆 Integrated pagination functionality with book category filtering. 🔄 Refactored home view for improved code readability and maintainability.

--- core/views.py
+++ core/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from catagories.models import Category_model
 from books.models import Book_model
 
 # Create your views here.
-def home(request,book_model_slug=None):
-
-    book_model_detials = Book_model.objects.all()
+def home(request, book_model_slug=None):
+    book_model_details = Book_model.objects.all()
     if book_model_slug is not None:
-        book_mode_slug_field= Category_model.objects.get(slug=book_model_slug)
-        book_model_detials= Book_model.objects.filter(categories=book_mode_slug_field)
+        book_model_slug_field = Category_model.objects.get(slug=book_model_slug)
+        book_model_details = Book_model.objects.filter(categories=book_model_slug_field)
 
-    catagory_details = Category_model.objects.all()
-    return render(request,"./home.html",{'catagory_details':catagory_details,'book_model_detials':book_model_detials})
+    category_details = Category_model.objects.all()
 
+    # Pagination
+    paginator = Paginator(book_model_details, 6)  # Show 6 books per page.
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
+    return render(request, "./home.html", {'category_details': category_details, 'page_obj': page_obj})
 
 
 
