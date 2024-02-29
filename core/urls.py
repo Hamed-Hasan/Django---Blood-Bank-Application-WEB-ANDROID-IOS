b49e691c959a32ae0279b86ae9d13c7a9b21c9d3
Hamed Hasan <hamedhasan422421@gmail.com>
2024-02-29 21:38:53 +0300

    Updated home view to include search functionality.  Modified home view to handle search queries within the same page. 🛠️ Integrated search functionality alongside category buttons in the template. 👀 Updated template to retain search query in the input field after submission. 📚 Implemented pagination for search results, ensuring a seamless user experience.

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,9 @@
 
 from django.urls import path,include
 from . import views
-
+from .views import home
 
 urlpatterns = [
     path('', views.home,name="home" ),
-    
+    path('<slug:book_model_slug>/', home, name='home'), 
 ]
--- core/views.py
+++ core/views.py
@@ -5,18 +5,25 @@ from books.models import Book_model
 from django.core.paginator import Paginator
 
 def home(request, book_model_slug=None):
-    book_model_details = Book_model.objects.all()
-    if book_model_slug is not None:
-        book_model_slug_field = Category_model.objects.get(slug=book_model_slug)
+    category_details = Category_model.objects.all()
+    if book_model_slug:
+        # Use get_object_or_404 to avoid DoesNotExist exception
+        book_model_slug_field = get_object_or_404(Category_model, slug=book_model_slug)
         book_model_details = Book_model.objects.filter(categories=book_model_slug_field)
+    else:
+        book_model_details = Book_model.objects.all()
+
+    query = request.GET.get('q')
+    if query:
+        book_model_details = book_model_details.filter(title__icontains=query)
 
     # Pagination
     paginator = Paginator(book_model_details, 6)  # Show 6 books per page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    category_details = Category_model.objects.all()
-    return render(request, "home.html", {'category_details': category_details, 'page_obj': page_obj})
-
-
-
+    return render(request, "home.html", {
+        'category_details': category_details,
+        'page_obj': page_obj,
+        'query': query
+    })
